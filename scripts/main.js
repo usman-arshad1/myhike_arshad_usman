42dac0a9e624be0f9ceabbf71c5d8017fe80c48a
Usman Arshad <uarshad2012@gmail.com>
2022-03-01 10:40:25 -0800

    updated images for demo9

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,50 @@ function insertName() {
      });
  }
 insertName();
+
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeHikeData() {
+     max = 3;
+     //define a variable for the collection you want to create in Firestore to populate data
+     var hikesRef = db.collection("hikes");
+     for (i = 1; i <= max; i++) {
+         hikesRef.add({ //add to database, autogen ID
+             code: "id" + i,
+             name: "hike" + i,
+             details: "Usman says this hike is amazing!  You will love going on hike" + i
+         })
+    }
+ }
+ 
+
+ function displayCards(collection) {
+     let cardTemplate = document.getElementById("hikeCardTemplate");
+ 
+     db.collection(collection).get()
+         .then(snap => {
+             var i = 1;
+             snap.forEach(doc => { //iterate thru each doc
+                 var title = doc.data().name;   // get value of the "name" key
+                 var details = doc.data().details;   // get value of the "details" key
+                 let newcard = cardTemplate.content.cloneNode(true);
+ 
+                 //update title and text and image
+                 newcard.querySelector('.card-title').innerHTML = title;
+                 newcard.querySelector('.card-text').innerHTML = details;
+                 newcard.querySelector('.card-image').src = "./images/" + collection + ".jpg"; //hikes.jpg
+ 
+                 //give unique ids to all elements for future use
+                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                 // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+ 
+                 //attach to gallery
+                 document.getElementById(collection + "-go-here").appendChild(newcard);
+                 i++;
+             })
+         })
+ }
+ 
+ displayCards("hikes");
